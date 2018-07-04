2f2a0b079cf1caa67945fe80a364749e0ae3a4b3
Dirk Barbi <dirk.barbi@awi.de>
2018-07-04 13:17:53 +0200

    Small changes to link recom library

---
+++ cmake/FindREcoM.cmake
@@ -0,0 +1,3 @@
+find_path(RECOM_Fortran_INCLUDE_DIRECTORIES recom_ocean_settings.mod HINTS ${TOPLEVEL_DIR}/../recom/build)
+find_library(RECOM_Fortran_LIBRARIES recom HINTS ${TOPLEVEL_DIR}/../recom/build)
+
